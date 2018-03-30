c8e373f738bc6e33a516591c2900570f63718810
Frieeze <alexis.surbayrole@isen.yncrea.fr>
2018-03-30 11:19:29 +0200

    ca va marcher a la fin

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String [ ] args) {
 
-		System.out.println(" Voila le badminton classement : 1er - 2eme - 3eme");
+		System.out.println(" Voila le badminton classement ");
 
 	}
 }
