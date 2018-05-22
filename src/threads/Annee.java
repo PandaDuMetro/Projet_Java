8e9d93362214752ac2384c67852bc7af05efab5c
PandaDuMetro <thomas.briquet@isen-lille.fr>
2018-05-22 15:27:05 +0200

    Ajout scene Player

--- src/threads/Annee.java
+++ src/threads/Annee.java
@@ -11,8 +11,8 @@ public class Annee extends Thread {
 	
 	public Annee(ArrayList<Player> players) {
 		super();
-		this.players = players; //on crée 6 tournois pour tout le long de l'année
-		this.tournois.add(new Tournoi("tournoi 1",this.players)); //fait à l'arrache
+		this.players = players; //on cree 6 tournois pour tout le long de l'annee
+		this.tournois.add(new Tournoi("tournoi 1",this.players)); //fait a l'arrache
 		this.tournois.add(new Tournoi("tournoi 2",this.players)); //voir si ca peut pas etre plus propre
 		this.tournois.add(new Tournoi("tournoi 3",this.players));
 		this.tournois.add(new Tournoi("tournoi 4",this.players));
@@ -24,13 +24,13 @@ public class Annee extends Thread {
 
 	@Override
 	public void run() {
-		//remettre les classements à 1 pour le début d'année
+		//remettre les classements a 1 pour le debut d'annee
 		for (Player elt: this.players) {
             elt.setRank(1);
         }
 		//lancer tout les tournois 1 a 1
 		//requete nouveau tournoi
-		//attendre fin d'un tournoi pour lancer le prochain
+		//attendre fin dU tournoi pour lancer le prochain
 		for(int i = 0; i < 6; i++) {
 			this.tournois.get(i).start();
 			try {
--- src/threads/Tournoi.java
+++ src/threads/Tournoi.java
@@ -26,15 +26,15 @@ public class Tournoi extends Thread {
 		ExecutorService exec;
 		//random sur le tableau des joueurs
 		Collections.shuffle(this.players);
-		//boucle while jusqu'à 1 joueur restant
+		//boucle while jusqu'a 1 joueur restant
 		int ronde = 0;
 		while(this.players.size() != 1) {
 			ronde++;
-			//chaque ronde : tableau de matchs joueurs 2 à deux
+			//chaque ronde : tableau de matchs joueurs 2 a deux
 			List<Match> matchs = new ArrayList<Match>();
-			for(int i = 0; i < 64/((int)Math.pow(2,ronde)); i++) { //on crée les matchs avec deux joueurs
+			for(int i = 0; i < 64/((int)Math.pow(2,ronde)); i++) { //on cree les matchs avec deux joueurs
 				matchs.add(new Match(this.players.get(0), this.players.get(1), ronde));
-				this.players.remove(0); //puis on retire les joueurs du tableau pour ne pas les réutiliser avant la prochaine ronde
+				this.players.remove(0); //puis on retire les joueurs du tableau pour ne pas les reutiliser avant la prochaine ronde
 				this.players.remove(1);
 			}
 			//lancer tout les matchs via un executor
@@ -46,7 +46,7 @@ public class Tournoi extends Thread {
 			while(!exec.isTerminated()) {
 				System.out.println("running"); //attente des threads
 			}
-			//on récupère les vainqueurs pour la prochaine ronde
+			//on recupere les vainqueurs pour la prochaine ronde
 			this.players.clear();
 			this.players = matchs.stream().map(x -> x.getWinner()).collect(Collectors.toList()); 
 		}
